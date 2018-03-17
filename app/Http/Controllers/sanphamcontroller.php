3aee2bb26ef8306ed9532c288aaf2c90dd171830
NguyenGiaBao1560036 <34787016+NguyenGiaBao1560036@users.noreply.github.com>
2018-03-17 23:42:09 +0700

    them san pham co check dieu kien

--- app/Http/Controllers/sanphamcontroller.php
+++ app/Http/Controllers/sanphamcontroller.php
@@ -11,4 +11,55 @@ class sanphamcontroller extends Controller
         $sanpham = Product::orderBy('id','DESC')->get();
         return view('admin.sanpham.danhsach',['sanpham'=>$sanpham]);
     }
+    public function getthem(){
+        $loaisanpham = ProductType::all();
+        return view('admin.sanpham.them',['loaisanpham'=>$loaisanpham]);
+    }
+
+    public function postthem(Request $request){
+        $this->validate($request,[
+            'loaisanpham'=>'required',
+            'name'=>'required|min:3|unique:products,name',
+            'description'=>'required',
+            'unit_price'=>'required',
+            'promotion_price'=>'required',
+
+        ],[
+            'loaisanpham.required'=>'Bạn chưa chọn loại sản phẩm',
+            'name.required'=>'Bạn chưa nhập tên sản phẩm',
+            'name.min'=>'Sản phẩm phải có ít 3 ký tự ',
+            'name.unique'=>'Tiêu đề đã tồn tại',
+            'description.required'=>'Bạn chưa nhập miêu tả',
+            'unit_price.required'=>'Bạn chưa nhập giá gốc',
+            'promotion_price.required'=>'Bạn chưa nhập khuyến mãi'
+            
+
+        ]);
+
+        $sanpham = new Product;
+        $sanpham->name= $request->name;
+        $sanpham->id_type=$request->loaisanpham;
+        $sanpham->description = $request->description;
+        $sanpham->unit_price= $request->unit_price;
+        $sanpham->promotion_price = $request->promotion_price;
+        $sanpham->new = $request->new;
+        if($request ->hasFile('image'))
+        {
+            $file = $request->file('image');
+            $name = $file->getClientOriginalName();
+            $image = str_random(4)."_". $name;
+            while(file_exists("Source/image/product" .$image))
+            {
+                $image = str_random(4)."_". $name;
+            }
+            $file->move("Source/image/product",$image);
+            $sanpham->image = $image;
+        }
+        else
+        {
+            $sanpham->image="";
+        }
+        $sanpham->save();
+        return redirect('admin/sanpham/them')->with('thongbao','Thêm sản phẩm thành công');
+    }
 }

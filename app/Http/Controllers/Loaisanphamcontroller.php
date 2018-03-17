731ef8b121fdfe934890b749c9df485b77be0bda
CHU VIET DUNG <1560103@student.hcmus.edu.vn>
2018-03-17 16:06:43 +0700

    ckeck loi khi them loai san pham

--- app/Http/Controllers/Loaisanphamcontroller.php
+++ app/Http/Controllers/Loaisanphamcontroller.php
@@ -12,4 +12,29 @@ class Loaisanphamcontroller extends Controller
         $loaisanpham = ProductType::all();
         return view('admin.loaisanpham.danhsach',['loaisanpham'=>$loaisanpham]);
     }
+    public function getthem(){
+        return view('admin.loaisanpham.them');
+    }
+
+    public function postthem(Request $req){
+        $this->validate($req,
+        [
+            'name'=>'required|unique:ProductType,name|min:2|max:100',
+            'description'=>'required',
+        ],
+        [
+            'name.required'=>'Bạn Chưa Nhập Loại Sản Phẩm',
+            'name.unique'=>'Tên loại sản phẩm đã tồn tại',
+            'name.min'=>'Tên loại sản phẩm phải có ít nhất .min kí tự ',
+            'name.max'=>'Tên loại sản phẩm phải có nhiều nhất .max kí tự',
+            'description.required'=>'Bạn Chưa Nhập miêu tả',
+        ]);
+
+        $loaisanpham = new ProductType;
+        $loaisanpham->name = $req->name;
+        $loaisanpham->description = $req->description;
+        $loaisanpham->save();
+
+        return redirect('admin/loaisanpham/them')->with('thongbao','Thêm Thành Công');
+    }
 }
--- resources/views/admin/loaisanpham/them.blade.php
+++ resources/views/admin/loaisanpham/them.blade.php
@@ -1,47 +1,45 @@
 @extends('admin.layout.index')
 @section('content')
 <div id="page-wrapper">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Category
-                    <small>List</small>
-                </h1>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Loại Sản Phẩm
+                            <small>Thêm</small>
+                        </h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                    <div class="col-lg-7" style="padding-bottom:120px">
+                        @if(count($errors) > 0)
+                            <div class="alert alert-danger">
+                                @foreach($errors ->all() as $err)
+                                    {{$err}}<br>
+                                @endforeach
+                            </div>
+                        @endif
+
+                        @if(session('thongbao'))
+                            <div class="alert alert-success">
+                                {{session('thongbao')}}
+                            </div>
+                        @endif
+                        <form action="admin/loaisanpham/them" method="POST">
+                        <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
+                            <div class="form-group">
+                                <label>Tên Loại Sản Phẩm</label>
+                                <input class="form-control" name="name" placeholder="Nhập Tên Loại Sản Phẩm" />
+                            </div>
+                            <div class="form-group">
+                                <label>Miểu tả</label>
+                                <input class="form-control" name="description" placeholder="Nhập miêu tả" />
+                            </div>
+                            <button type="submit" class="btn btn-default">Thêm</button>
+                            <button type="reset" class="btn btn-default">Làm Mới</button>
+                        <form>
+                    </div>
+                </div>
+                <!-- /.row -->
             </div>
-            <!-- /.col-lg-12 -->
-            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                <thead>
-                    <tr align="center">
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Category Parent</th>
-                        <th>Status</th>
-                        <th>Delete</th>
-                        <th>Edit</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="odd gradeX" align="center">
-                        <td>1</td>
-                        <td>Tin Tức</td>
-                        <td>None</td>
-                        <td>Hiện</td>
-                        <td class="center"><i class="fa fa-trash-o  fa-fw"></i><a href="#"> Delete</a></td>
-                        <td class="center"><i class="fa fa-pencil fa-fw"></i> <a href="#">Edit</a></td>
-                    </tr>
-                    <tr class="even gradeC" align="center">
-                        <td>2</td>
-                        <td>Bóng Đá</td>
-                        <td>Thể Thao</td>
-                        <td>Ẩn</td>
-                        <td class="center"><i class="fa fa-trash-o  fa-fw"></i><a href="#"> Delete</a></td>
-                        <td class="center"><i class="fa fa-pencil fa-fw"></i> <a href="#">Edit</a></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <!-- /.row -->
-    </div>
-    <!-- /.container-fluid -->
+            <!-- /.container-fluid -->
 </div>
 @endsection

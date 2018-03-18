04afc21a3097c34490930340fd48023ecfe54898
ltchanhpy <32243481+ltchanhpy@users.noreply.github.com>
2018-03-18 10:14:39 +0700

    them slide

--- app/Http/Controllers/slidecontroller.php
+++ app/Http/Controllers/slidecontroller.php
@@ -10,4 +10,30 @@ class slidecontroller extends Controller
         $slide = Slide::all();
         return view('admin.slide.danhsach',['slide'=>$slide]);
     }
+    public function getthem(){
+        $slide = Slide::all();
+        return view('admin.slide.them',['slide'=>$slide]);
+   }
+
+    public function postthem(Request $request){
+        $slide = new Slide;
+        if($request ->hasFile('image'))
+        {
+            $file = $request->file('image');
+            $name = $file->getClientOriginalName();
+            $image = str_random(4)."_". $name;
+            while(file_exists("Source/image/slide" .$image))
+            {
+                $image = str_random(4)."_". $name;
+            }
+            $file->move("Source/image/slide",$image);
+            $slide->image = $image;
+        }
+        else
+        {
+            $slide->image="";
+        }
+        $slide->save();
+        return redirect('admin/slide/them')->with('thongbao','Thêm slide thành công');
+   }
 }
